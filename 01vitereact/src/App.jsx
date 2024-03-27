d60f44b3874b68a957728bac8892fad8ae481db3
sushant kumar <ksushant0293@gmail.com>
2024-03-27 20:08:15 +0530

    learned how to use hooks

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,14 +2,15 @@ import Learn from "./Learn";
 
 function App() {
 
+  const username = "learning "
   return (
-  //  <h1>learn react with vite || sushant</h1>
+
   <>
   <Learn/>
-  <h1>learn with sushant</h1>
+  <h1>learn with sushant {username}</h1>
   <p>padh le bhai</p>
   </>
   )
-}
+}6  
 
 export default App
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+  // let counter = 5
+
+  const addValue = () => {
+    // counter = counter + 1;
+    setCounter(counter + 1)
+    // console.log("clicked", counter);
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+    <h1>hello world</h1>
+    <h2>counter value: {counter}</h2>
+    <button
+    onClick={addValue}
+    >Add value: {counter}</button>
+    <br />
+    <button
+    onClick={removeValue}
+    >remove value: {counter} </button>
+
+    </>
+  )
+}
+
+export default App
